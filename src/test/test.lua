1a40b3fa380898505c11a286fc612bdcadcd49ef
JiahaoWu <jiahaodev@163.com>
2017-05-17 18:13:30 +0800

    添加编程技巧skill.lua文件

--- src/test/test.lua
+++ src/test/test.lua
@@ -3,29 +3,43 @@
 --c = "3"
 --print(((a and b) or c))
 
-tab = {"a",_key3 = "v","c",[2]="b"}
-tab["1"] = "ff"
-tab["key1"] = "val1"
-
-tab2 = {"a","b",[2]="c",}
-
-for k,v in pairs(tab) do 
+--tab = {"a",_key3 = "v","c",[2]="b"}
+--tab["1"] = "ff"
+--tab["key1"] = "val1"
+--
+--tab2 = {"a","b",[2]="c",}
+--
+--for k,v in pairs(tab) do 
+----   print(k .. " - "..v)
+--end
+--print("******")
+--
+--for k,v in ipairs(tab) do 
 --   print(k .. " - "..v)
+--end
+--print("******")
+--
+--
+--for k,v in pairs(tab2) do 
+--   print(k .. " - "..v)
+--end
+--print("******")
+--
+--for k,v in ipairs(tab2) do 
+--   print(k .. " - "..v)
+--end
+
+
+
+for i = 1, 100 do
+  repeat 
+    if(i==15) then
+      break;        --这里实现其他语言中continue的逻辑
+    end  
+    --执行正常运算
+    
+    print("current num is :"..i)
+  
+  until true
+  
 end
-print("******")
-
-for k,v in ipairs(tab) do 
-   print(k .. " - "..v)
-end
-print("******")
-
-
-for k,v in pairs(tab2) do 
-   print(k .. " - "..v)
-end
-print("******")
-
-for k,v in ipairs(tab2) do 
-   print(k .. " - "..v)
-end
-
